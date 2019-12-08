5ebb2c6e80eccacfe4f6ee9b13278898dabec0f1
RedRem95 <alexander.vollmer.95@hotmail.de>
2019-12-08 07:25:23 +0100

    Implemented Day07 A2

---
+++ Day06/task.py
@@ -0,0 +1,123 @@
+from Day06 import INPUT
+
+custom_print = print
+
+
+class SpaceObject:
+    __space_objects = {}
+
+    def __init__(self, name, *orbits):
+        self.__name = str(name)
+        self.__orbits = [x for x in orbits]
+        self.__moons = []
+        for x in self.__orbits:
+            x.add_moons(self)
+
+        SpaceObject.__space_objects[name] = self
+
+    def add_orbit(self, *names):
+        self.__orbits.extend(names)
+        for x in [SpaceObject.get_space_object(name) for name in names]:
+            x.add_moons(self)
+
+    def set_orbit(self, *names):
+        self.__orbits = []
+        self.add_orbit(*names)
+
+    def __get_orbit_space_object(self):
+        return [x for x in self.__orbits if x is not None and isinstance(x, SpaceObject)]
+
+    def get_direct_orbits(self):
+        return self.__get_orbit_space_object()
+
+    def get_moons(self):
+        return [x for x in self.__moons if x is not None and isinstance(x, SpaceObject)]
+
+    def add_moons(self, *moon):
+        self.__moons.extend([x for x in moon])
+
+    def set_moons(self, *moon):
+        self.__moons = []
+        self.add_moons(*moon)
+
+    def get_indrect_orbits(self):
+        objects = []
+        for obj in self.__get_orbit_space_object():
+            objects.extend(obj.get_direct_orbits())
+            objects.extend(obj.get_indrect_orbits())
+        return objects
+
+    def get_all_orbits(self):
+        ret = self.get_direct_orbits().copy()
+        ret.extend(self.get_indrect_orbits())
+        return ret
+
+    def get_indirect_moons(self):
+        objects = []
+        for obj in self.get_moons():
+            objects.extend(obj.get_moons())
+            objects.extend(obj.get_indirect_moons())
+        return objects
+
+    def get_all_moons(self):
+        ret = self.get_moons().copy()
+        ret.extend(self.get_indirect_moons())
+        return ret
+
+    def find_ways(self, target, traveled=[], ways=[], only_first=True):
+        if self in traveled:
+            return []
+        traveled.append(self)
+        if SpaceObject.get_space_object(target) == self:
+            ways.append(traveled.copy())
+            return ways
+
+        for x in self.get_moons() + self.get_direct_orbits():
+            way_len_prev = len(ways)
+            SpaceObject.get_space_object(x).find_ways(target, traveled=traveled.copy(), ways=ways)
+            if way_len_prev < len(ways) and only_first:
+                return ways
+
+        return ways
+
+    @staticmethod
+    def get_space_object(name):
+        if isinstance(name, SpaceObject):
+            return name
+        if name not in SpaceObject.__space_objects:
+            SpaceObject(name)
+        return SpaceObject.__space_objects[name]
+
+    @staticmethod
+    def get_all_space_objects():
+        return [x for x in SpaceObject.__space_objects.values()]
+
+    def __str__(self):
+        return self.__name
+
+
+SpaceObject("COM")
+
+
+def parse():
+    for planet, moon in [x.split(")") for x in INPUT]:
+        SpaceObject.get_space_object(moon).set_orbit(SpaceObject.get_space_object(planet))
+
+
+def main(printer=print):
+    global custom_print
+    custom_print = printer
+    parse()
+    custom_print("\tA1:")
+    custom_print("\t\tAll the space objects:",
+                 sum((len(x.get_all_orbits()) for x in SpaceObject.get_all_space_objects())))
+
+    all_the_ways = sorted(
+        [x for x in SpaceObject.get_space_object("YOU").find_ways(SpaceObject.get_space_object("SAN"))],
+        key=lambda x: len(x))
+    shortest_way = all_the_ways[0]
+    count_transfers = max(0, len(shortest_way) - 3)
+    custom_print("\tA2:")
+    custom_print("\t\tNeeded transfers:", count_transfers)
+    custom_print("\t\t    Shortest way:",
+                 "->".join((str(x) for x in shortest_way[1:-1])) if count_transfers > 0 else "<You are already there>")
--- Day07/task.py
+++ Day07/task.py
@@ -1,4 +1,6 @@
-from typing import List, Callable, Tuple, Iterable
+import threading
+from queue import Queue
+from typing import List, Callable, Tuple, Iterable, Union
 
 from Day02.task import work_code, IntMachine, Mode
 from Day05.task import my_mode_machine
@@ -6,10 +8,6 @@ from Day07 import INPUT
 from helper import get_all_combs
 
 custom_printer = print
-
-my_mode_machine = my_mode_machine.copy()
-
-INPUT = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
 
 
 class Signal:
@@ -48,7 +46,6 @@ def check_amplifier(code: str, machine: IntMachine, phases: Iterable[int] = [], 
             return False, loc + 2
 
         def write(code: List[int], loc: int, modes: Callable[[int], Mode]) -> Tuple[bool, int]:
-            # print_int(loc + 1, code[code[loc + 1]])
             signal.set_signal(int(modes(1).read(code, loc + 1)))
             return False, loc + 2
 
@@ -59,23 +56,74 @@ def check_amplifier(code: str, machine: IntMachine, phases: Iterable[int] = [], 
     return signal.get_signal()
 
 
-def check_amplifier_rep(code: str, machine: IntMachine, phases: Iterable[int] = [], signal: int = 0):
-    signal = Signal(signal)
+__lock = threading.Lock()
 
-    def read(code: List[int], loc: int, modes: Callable[[int], Mode]) -> Tuple[bool, int]:
-        global __it
+
+class IntCodeThread(threading.Thread):
+
+    def __init__(self, my_id: int, max_ids: int, my_queue: Queue, target_queue: Queue,
+                 my_code: Union[str, List[int]], my_machine):
+        super().__init__(name="InCodeThread->%s" % my_id)
+        self.my_id = my_id
+        self.__max_ids = max_ids
+        self.__my_queue = my_queue
+        self.__my_code = my_code
+        self.__my_machine = my_machine
+        self.__target_queue = target_queue
+
+    def read(self, code_loc: List[int], loc: int, modes: Callable[[int], Mode]) -> Tuple[bool, int]:
         # code[code[loc + 1]] = read_int()
-        modes(1).write(code, loc + 1, data[__it % 2])
-        __it += 1
+        data = self.__my_queue.get()
+        modes(1).write(code_loc, loc + 1, data)
         return False, loc + 2
 
-    def write(code: List[int], loc: int, modes: Callable[[int], Mode]) -> Tuple[bool, int]:
+    def write(self, code_loc: List[int], loc: int, modes: Callable[[int], Mode]) -> Tuple[bool, int]:
         # print_int(loc + 1, code[code[loc + 1]])
-        signal.set_signal(int(modes(1).read(code, loc + 1)))
+        data = int(modes(1).read(code_loc, loc + 1))
+        self.__target_queue.put(data)
         return False, loc + 2
 
-    machine.register_action(3, read)
-    machine.register_action(4, write)
+    def run(self) -> None:
+        self.__my_machine.register_action(3, self.read)
+        self.__my_machine.register_action(4, self.write)
+        work_code(self.__my_code, self.__my_machine)
+
+
+def check_amplifier_rep(code: str, machine: IntMachine, phases: List[int] = [], signal: int = 0):
+    threads = []
+    queues: List[Queue[int]] = []
+
+    for i, phase in enumerate(phases):
+        my_queue: Queue[int] = Queue()
+        my_queue.put(phase)
+        if i == 0:
+            my_queue.put(signal)
+        queues.append(my_queue)
+
+    class CustomInt:
+
+        def __init__(self, value):
+            self.__value = value
+
+        def set_value(self, value):
+            self.__value = value
+
+        def get_value(self):
+            return self.__value
+
+    for j in range(len(phases)):
+        thread = IntCodeThread(my_id=j, max_ids=len(queues), my_queue=queues[j],
+                               target_queue=queues[(j + 1) % len(queues)], my_code="%s" % code,
+                               my_machine=machine.copy())
+
+        thread.start()
+
+        threads.append(thread)
+
+    for t in threads:
+        t.join()
+
+    return queues[0].get()
 
 
 def main(printer: print):
@@ -95,7 +143,7 @@ def main(printer: print):
     max_comp = []
     custom_printer("A2")
     for x in get_all_combs(minimum=5, maximum=9):
-        amp = check_amplifier(INPUT, my_mode_machine, x, 0)
+        amp = check_amplifier_rep(INPUT, my_mode_machine, x, 0)
         if amp > max_amp:
             max_amp = amp
             max_comp = x
